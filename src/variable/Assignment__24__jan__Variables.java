2c1e51b5d1bd8489d417a24b8a9e04a778435761
amandev2023 <devboyzzz@gmailcom>
2023-01-26 15:56:36 +0530

    Assignment for advance variables

---
+++ src/variable/Assignment__24__jan__Variables.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class Assignment__24__jan__Variables {
+
+    public static void main(String args[]){
+
+        int a=3;
+        int b=5;
+        int d=120;
+        int e=10;
+
+        // 1.Find the square of 3 //
+
+        int multiply=a*a;
+        System.out.println("Square of 3 is " +multiply);
+
+        //Find the percent of given number//
+
+
+    }
+}
