fdef3c7277db05a3e9d27afc2d2d2be568d4f4a9
amandev2023 <devboyzzz@gmailcom>
2023-01-26 16:30:53 +0530

    Assignment for advance variables

--- src/variable/Assignment__24__jan__Variables.java
+++ src/variable/Assignment__24__jan__Variables.java
@@ -2,19 +2,28 @@ package variable;
 
 public class Assignment__24__jan__Variables {
 
-    public static void main(String args[]){
+    public static void main(String[] args) {
 
-        int a=3;
-        int b=5;
-        int d=120;
-        int e=10;
+        int a = 3;
+        int b = 5;
 
         // 1.Find the square of 3 //
 
-        int multiply=a*a;
-        System.out.println("Square of 3 is " +multiply);
+        int multiply = a * a;
+        System.out.println("Square of 3 is " + multiply);
 
-        //Find the percent of given number//
+        //Find the cube of 5//
+
+        int multi = b * b * b;
+        System.out.println("Cube of 5 is : " + multi);
+
+        // 3.Find the percentage of given number//
+        //Find 20% of 120//
+
+        int number = 120;
+        int percent = 20;
+        int result = (number * percent) / 100;
+        System.out.println("20% of 120 is : " + result);
 
 
     }
